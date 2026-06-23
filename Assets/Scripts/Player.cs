d45efeb79e8e60e479fe0044505d28d377b29166
diego85 <149547878+Dieart38@users.noreply.github.com>
2026-06-23 13:38:37 -0300

    Add Camera e efeito de roll do player 23-06-26

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,4 +1,116 @@
-﻿using System.Collections;
+﻿// using System.Collections;
+// using System.Collections.Generic;
+// using System.Runtime.Serialization;
+// using UnityEngine;
+
+// public class Player : MonoBehaviour
+// {
+//     [SerializeField] private float speed;
+//     [SerializeField] private float runSpeed;
+//     [SerializeField] private float rollSpeed;
+
+//     private float currentSpeed;
+//     private bool _isRunning;
+//     private bool _isRolling;
+
+//     // Variável de controle para garantir que o jogador soltou o botão
+//     private bool _canRollAgain = true;
+
+//     private Rigidbody2D rb;
+//     private Vector2 _direction;
+
+//     public Vector2 direction
+//     {
+//         get { return _direction; }
+//         set { _direction = value; }
+//     }
+
+//     public bool isRunning
+//     {
+//         get { return _isRunning; }
+//         set { _isRunning = value; }
+//     }
+
+//     public bool isRolling
+//     {
+//         get { return _isRolling; }
+//         set { _isRolling = value; }
+//     }
+
+//     void Start()
+//     {
+//         currentSpeed = speed;
+//         rb = GetComponent<Rigidbody2D>();
+//     }
+//     void Update()
+//     {
+//         OnInput();
+//         OnRun();
+//         OnRoll();
+//     }
+//     void FixedUpdate()
+//     {
+//         OnMove();
+//     }
+
+//     #region Movement
+
+//     void OnInput()
+//     {
+//         _direction = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
+//     }
+
+//     void OnMove()
+//     {
+//         //rb.velocity = new Vector2(direction.x * speed, direction.y * speed);
+//         rb.MovePosition(rb.position + _direction * speed * Time.fixedDeltaTime);
+//     }
+
+//     void OnRun()
+//     {
+//         // ele so corre se estiver andando parado não pode correr
+//         if (Input.GetKeyDown(KeyCode.LeftShift) && _direction != Vector2.zero)
+//         {
+//             Debug.Log("Running");
+//             speed = runSpeed;
+//             _isRunning = true;
+//         }
+//         if (Input.GetKeyUp(KeyCode.LeftShift))
+//         {
+//             speed = currentSpeed;
+//             _isRunning = false;
+//         }
+//     }
+
+//     void OnRoll()
+//     {
+//         if (Input.GetMouseButtonDown(1) && _direction != Vector2.zero && _canRollAgain)
+//         {
+//             speed = rollSpeed;
+//             Debug.Log("Rolling");
+//             _isRolling = true;
+//             _canRollAgain = false;
+
+//         }
+//         if (_isRolling)
+//         {
+//             // Exemplo lógico: Você deve voltar a velocidade ao normal após o tempo do rolamento terminar,
+//             // e não baseando-se no GetMouseButtonUp do jogador.
+//             speed = currentSpeed;
+//             _isRolling = false;
+//         }
+//         if (Input.GetMouseButtonUp(1))
+//         {
+
+//             _canRollAgain = true;
+
+//         }
+
+//     }
+//     #endregion
+// }
+
+using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.Serialization;
 using UnityEngine;
@@ -8,13 +120,19 @@ public class Player : MonoBehaviour
     [SerializeField] private float speed;
     [SerializeField] private float runSpeed;
     [SerializeField] private float rollSpeed;
+    [SerializeField] private float rollDuration = 0.5f; // Tempo de duração do rolamento em segundos
 
     private float currentSpeed;
     private bool _isRunning;
     private bool _isRolling;
 
+    // Variável de controle para garantir que o jogador soltou o botão
+    private bool _canRollAgain = true;
+
     private Rigidbody2D rb;
     private Vector2 _direction;
+
+    private PlayerAnim anim; // Referência ao PlayerAnim para controlar animações
 
     public Vector2 direction
     {
@@ -38,13 +156,16 @@ public class Player : MonoBehaviour
     {
         currentSpeed = speed;
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<PlayerAnim>();
     }
+
     void Update()
     {
         OnInput();
         OnRun();
         OnRoll();
     }
+
     void FixedUpdate()
     {
         OnMove();
@@ -54,18 +175,23 @@ public class Player : MonoBehaviour
 
     void OnInput()
     {
-        _direction = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
+        // Se estiver rolando, podemos travar o input para ele não mudar de direção no meio do rolamento (opcional)
+        if (!_isRolling)
+        {
+            _direction = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
+        }
     }
 
     void OnMove()
     {
-        //rb.velocity = new Vector2(direction.x * speed, direction.y * speed);
         rb.MovePosition(rb.position + _direction * speed * Time.fixedDeltaTime);
     }
 
     void OnRun()
     {
-        // ele so corre se estiver andando parado não pode correr
+        // Não deixa correr se já estiver rolando
+        if (_isRolling) return;
+
         if (Input.GetKeyDown(KeyCode.LeftShift) && _direction != Vector2.zero)
         {
             Debug.Log("Running");
@@ -81,20 +207,48 @@ public class Player : MonoBehaviour
 
     void OnRoll()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        // Só inicia o rolamento se apertar o botão, estiver se movendo, não estiver rolando E puder rolar de novo
+        if (Input.GetMouseButtonDown(1) && _direction != Vector2.zero && _canRollAgain && !_isRolling)
         {
-            speed = rollSpeed;
-            Debug.Log("Rolling");
-            _isRolling = true;
-
+            StartCoroutine(RollRoutine());
         }
-        if (Input.GetKeyUp(KeyCode.Space))
+
+        // Se soltar o botão do mouse, libera para poder rolar no próximo clique
+        if (Input.GetMouseButtonUp(1))
+        {
+            _canRollAgain = true;
+        }
+    }
+
+    // Corrotina que controla o tempo do rolamento de forma independente do clique do mouse
+    private IEnumerator RollRoutine()
+    {
+        _isRolling = true;
+        _canRollAgain = false; // Bloqueia novos comandos
+        _isRunning = false;    // Cancela a corrida se estivesse correndo
+        
+        speed = rollSpeed;     // Aplica a velocidade do rolamento
+        Debug.Log("Rolling Started");
+
+        anim.TriggerRollAnimation();
+
+        // Aguarda a duração estipulada do rolamento
+        yield return new WaitForSeconds(rollDuration);
+
+        // O rolamento acabou: volta o estado original baseado se o Shift está pressionado ou não
+        if (Input.GetKey(KeyCode.LeftShift) && _direction != Vector2.zero)
+        {
+            speed = runSpeed;
+            _isRunning = true;
+        }
+        else
         {
             speed = currentSpeed;
-            _isRolling = false;
-
         }
 
+        _isRolling = false;
+        Debug.Log("Rolling Ended");
     }
+
     #endregion
 }
--- Assets/Scripts/PlayerAnim.cs
+++ Assets/Scripts/PlayerAnim.cs
@@ -17,9 +17,17 @@ public class PlayerAnim : MonoBehaviour
 
     void Update()
     {
+        // Se o jogador estiver rolando, nós evitamos que as animações de 
+        // andar/correr fiquem se sobrepondo à animação de rolamento.
+        if (player.isRolling)
+        {
+            // Opcional: Mantém a rotação do sprite mesmo rolando
+            HandleSpriteFlip(); 
+            return; 
+        }
+
         OnMove();
         OnRun();
-        //OnRoll();
 
     }
 
@@ -28,41 +36,42 @@ public class PlayerAnim : MonoBehaviour
     {
         if (player.direction.sqrMagnitude > 0)
         {
-            if (player.isRolling)
-            {
-                anim.SetTrigger("isRoll");
-            }
-            anim.SetInteger("transition", 1);
+            anim.SetInteger("transition", 1); // Andando
         }
         else
         {
-            anim.SetInteger("transition", 0);
+            anim.SetInteger("transition", 0); // Parado (Idle)
         }
 
-        if (player.direction.x > 0)
-        {
-            transform.eulerAngles = new Vector2(0, 0);
-        }
-        if (player.direction.x < 0)
-        {
-            transform.eulerAngles = new Vector2(0, 180);
-        }
+        HandleSpriteFlip();
     }
 
     void OnRun()
     {
         if (player.isRunning)
         {
-            anim.SetInteger("transition", 2);
+            anim.SetInteger("transition", 2); // Correndo
         }
     }
 
-    // void OnRoll()
-    // {
-    //     if (player.isRolling)
-    //     {
-    //         anim.SetTrigger("isRoll");
-    //     }
-    // }
+    // Isolamos a lógica de virar o sprite para ficar mais limpo
+    void HandleSpriteFlip()
+    {
+        if (player.direction.x > 0)
+        {
+            transform.eulerAngles = new Vector3(0, 0, 0);
+        }
+        else if (player.direction.x < 0)
+        {
+            transform.eulerAngles = new Vector3(0, 180, 0);
+        }
+    }
+
+    // ESTA FUNÇÃO DEVE SER CHAMADA PELO SCRIPT DO PLAYER APENAS UMA VEZ!
+    public void TriggerRollAnimation()
+    {
+        anim.SetTrigger("isRoll");
+    }
     #endregion
 }
+
