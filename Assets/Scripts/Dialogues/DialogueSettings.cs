67b468f5ccdaa5d962fb9aca7b2791045fe8ba42
diego85 <149547878+Dieart38@users.noreply.github.com>
2026-06-24 12:11:08 -0300

    Add Npc-1 e sistema de dialogos iniciando 24-06-26

---
+++ Assets/Scripts/Dialogues/DialogueSettings.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Dialogue", menuName = "New Dialogue/Dialogue")]
+public class NewBehaviourScript : ScriptableObject
+{
+    [Header("Settings")]
+    // public float textSpeed = 0.05f;
+    // public Color textColor = Color.white;
+    public GameObject Actor;
+
+    [Header("Dialogue")]
+    public Sprite speakerSprite;
+    public string sentence;
+
+    public List<Sentence> dialogues = new List<Sentence>();
+}
+
+[System.Serializable]
+public class Sentence
+{
+    public string actorName;
+    public Sprite profile;
+    public Languages sentence;
+}
+
+[System.Serializable]
+public class Languages
+{
+    public string portuguese;
+
+    public string english;
+    public string spanish;
+
+}
+
+
